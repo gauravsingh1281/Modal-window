4a8c8a7ac2d53b7d176bfe507f42579cb7168369
Gaurav Pratap Singh <gs492979@gmail.com>
2024-03-04 01:22:30 +0530

    Modal window show hide functionality added.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+const showModal = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+const closeModal = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnsOpenModal.forEach((e) => e.addEventListener("click", showModal));
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) closeModal();
+});
